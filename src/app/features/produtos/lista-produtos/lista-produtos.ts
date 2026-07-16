29565471d43440c01807b67643b664c955c53e29
arthur da costa costa <arthurcosta2727@gmail.com>
2026-07-16 19:53:43 -0300

    refactor: utilizar Signals no gerenciamento da lista de produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Produto } from '../produto/produto';
+import {signal} from '@angular/core';
 @Component({
   selector: 'app-lista-produtos',
   imports: [Produto],
@@ -7,11 +8,11 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  produtos = [
+  produtos = signal( [
     {nome: 'attack shark x11', preco: 200.99},
     {nome: 'monitor ultraggear', preco: 980.99},
     {nome: 'ssd sata 521gb', preco: 300}
-  ];
+  ]);
     exibirProduto(nome: string){
       console.log('Produto Selecionado: ', nome);
     }
