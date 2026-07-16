181ce51c28686cf498aea164a002f04d4461a578
arthur da costa costa <arthurcosta2727@gmail.com>
2026-07-16 19:21:19 -0300

    refactor(features): move componentes Produto e ListaProdutos para features

---
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Produto } from '../produto/produto';
+@Component({
+  selector: 'app-lista-produtos',
+  imports: [Produto],
+  templateUrl: './lista-produtos.html',
+  styleUrl: './lista-produtos.css',
+})
+export class ListaProdutos {
+  produtos = [
+    {nome: 'attack shark x11', preco: 200.99},
+    {nome: 'monitor ultraggear', preco: 980.99},
+    {nome: 'ssd sata 521gb', preco: 300}
+  ];
+    exibirProduto(nome: string){
+      console.log('Produto Selecionado: ', nome);
+    }
+}
+
