5d8e91a2ead6be02e30476aeaf4c0da2b6307693
arthur da costa costa <arthurcosta2727@gmail.com>
2026-07-16 21:04:07 -0300

    feat: adicionar contador de produtos com computed

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { Produto } from '../produto/produto';
 import {signal} from '@angular/core';
+import { computed } from '@angular/core';
 @Component({
   selector: 'app-lista-produtos',
   imports: [Produto],
@@ -8,19 +9,24 @@ import {signal} from '@angular/core';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  //!Lista com dados - Array
   produtos = signal( [
     {nome: 'attack shark x11', preco: 200.99},
     {nome: 'monitor ultraggear', preco: 980.99},
     {nome: 'ssd sata 521gb', preco: 300}
   ]);
+  //!Função para exibir produtos selecionados pelo o usuario no console
     exibirProduto(nome: string){
       console.log('Produto Selecionado: ', nome);
     }
+    //!Função que adiciona produto usado metodo update
     adicionarProduto(){
       this.produtos.update(listaAtual => [
         ...listaAtual,
         {nome:"playstation 5", preco: 3000},
       ]);
     }
+    //!Função que contabiliza a quantidade de produtos na lista
+   totalProduto = computed(() => this.produtos(). length);
 }
 
