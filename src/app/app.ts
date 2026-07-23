1ca531db645979136cec4d1b9848ef3eea79e71d
arthur da costa <arthurcosta2727@gmail.com>
2026-07-22 21:25:42 -0300

    feat(auth): integrar autenticação ao componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 //import {Produto} from './components/produto/produto';//importando a classe produto do arquivo produto.ts para ser usado no componente app.ts
 import { UpperCasePipe } from '@angular/common';
+import { usuarioLogado,login, logout } from './core/auth';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, UpperCasePipe],
@@ -11,4 +12,7 @@ import { UpperCasePipe } from '@angular/common';
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'mercado bandido';
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
